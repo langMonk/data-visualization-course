4aad524fa5f6bc4c025d2ffef71cdc940eaa1f03
langMonk <lang.monk93@gmail.com>
2025-03-11 10:14:03 +0100

    lab 0: descriptive analytics

---
+++ content/topics/00-descriptive-analytics/_meta.js
@@ -0,0 +1,16 @@
+export default {
+    _: {
+        type: 'separator',
+        title: 'Descriptive Analytics'
+    },
+    index: "Introduction",
+    __: {
+        type: 'separator',
+        title: 'Exercises'
+    },
+    '01-ex1-data-preparation': 'Exercise 1: Data Preparation',
+    '02-ex2-univariate-analysis': 'Exercise 2: Univariate Analysis',
+    '03-ex3-multivariate-analysis': 'Exercise 3: Multivariate Analysis',
+    '04-ex4-multidimensional-visualization': 'Exercise 4: Multidimensional Visualization',
+    '05-ex5-comprehensive-analysis-project': 'Exercise 5: Comprehensive Analysis Project'
+}
