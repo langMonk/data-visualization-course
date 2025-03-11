8bbdbceb185f39322ef03c65ada172d9a7a9f674
Bartosz Minch <bartosz.minch@pwc.com>
2025-03-11 10:14:03 +0100

    lab 0: descriptive analytics

--- content/_meta.js
+++ content/_meta.js
@@ -9,8 +9,12 @@ export default {
         title: "Rozdziały",
         type: 'menu',
         items: {
+            "00-descriptivee-analytics": {
+                title: "1. Descriptive Analytics",
+                href: "/topics/00-descriptive-analytics"
+            },
             "01-pca-kernelpca": {
-                title: "1. PCA & kernelPCA",
+                title: "2. PCA & kernelPCA",
                 href: "/topics/01-pca-kernelpca"
             },
         }
--- content/topics/_meta.js
+++ content/topics/_meta.js
@@ -1,4 +1,8 @@
 export default {
+    "00-descriptive-analytics": {
+        title: "Descriptive Analytics",
+        type: "page"
+    },
     "01-pca-kernelpca": {
         title: "PCA & kernelPCA",
         type: "page"
